313b95d94b881320211beaec14a9b1b862439786
alehenaixiao111 <1420666079@qq.com>
2022-05-30 20:26:43 +0800

    完成了主页功能

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -4,4 +4,20 @@
 $.ajaxPrefilter((option) => {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     option.url = `http://www.liulongbin.top:3007` + option.url;
+    //给/my/相关接口注入token
+    if (option.url.includes("/my/")) {
+      option.headers = {
+          Authorization: localStorage.getItem("token"),
+      };
+  }
+  //每次发送请求回来校验 token 是否存在或过期
+  option.complete = (res)=>{
+    if(res.responseJSON.status ===1 &&
+    res.responseJSON.message === '身份认证失败！'){
+    //强制清空token
+    localStorage.removeItem('token')
+    //跳转登录页面
+    location.href = '/login.html'
+    }
+  }
   });
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+//获取用户的基本信息
+function getUserInfo(){
+    $.ajax({
+        type:'GET',
+        url:'/my/userinfo',
+        // headers:{
+        //     Authorization:localStorage.getItem('token')
+        // },
+        success:(res)=>{
+            if(res.status !=0)return layer.msg('获取用户信息失败');
+            layer.msg('获取用户信息成功');
+            renderAvatar(res.data)
+        }
+    });
+}
+    //渲染用户信息
+    const renderAvatar = (user) =>{
+        console.log(user);
+        let uname = user.nickname || user.username ;
+    //渲染欢迎语
+    $('#welcome').html(`欢迎 ${uname}`);
+    //按需渲染头像
+    if(user.user_pic !=null){
+    //设置图片头像
+    $('.layui-nav-img').attr('src',user.user_pic);
+    $('.text-avatar').hide();
+    }else{
+         //设置文本头像
+     $('.layui-nav-img').hide();
+     $('.text-avatar').html(uname[0].toUpperCase())
+    }
+    };
+    //退出功能
+    $('#btnlogout').click(()=>{
+        layer.confirm('哥哥真要走吗?', {icon: 3, title:'提示'},
+         function(index){
+        localStorage.removeItem('token');
+        location.href = '/login.html';
+          });
+    });
+
+
+
+
+
+
+
+
+  getUserInfo();
